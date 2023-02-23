b238c32af5a9b16bf25746e03955ac419e1aa1b7
hyunwoo <ashyonu@gmail.com>
2023-02-22 23:10:50 -0800

    feat: add DB

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'assignment',
+        'USER': 'root',
+        'PASSWORD': '0502',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
