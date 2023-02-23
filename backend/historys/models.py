8686bdc6078b8b4c682f065497ae05b8ac29b424
hyunwoo <ashyonu@gmail.com>
2023-02-23 03:47:07 -0800

    chore: add models.py

--- backend/historys/models.py
+++ backend/historys/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class history(models.Model):
+    history_id = models.BigAutoField(primary_key=True, null=False)
+    fk_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_spend = models.BooleanField(null=False)    # 지출인지 수입인지
+    cost = models.BigIntegerField(null=False)   # 변동된 금액
+    balance = models.BigIntegerField(null=False)    # 잔고
+    memo = models.CharField(max_length=100, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'history'
